241f5e620b206825da402834a8baa465632acd22
aceg224 <aceg224@gmail.com>
2019-01-27 16:46:09 +0800

    Initial Commit

---
+++ HW41/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW41
+//
+//  Created by min-chia on 2019/1/27.
+//  Copyright © 2019 min-chia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
